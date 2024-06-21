c8afb30988daa4b6c610b2a7477d9b1671746aa0
Paul Martinez <paulmartinez@hotmail.fr>
2024-06-21 17:07:02 +0200

    some style

--- forum/admin.py
+++ forum/admin.py
@@ -2,4 +2,29 @@ from django.contrib import admin
 
 from .models import Topic
 
-admin.site.register(Topic)
+from django.contrib import admin
+
+from .models import Topic, Comment
+
+# class CommentInline(admin.StackedInline):
+#     model = Comment
+#     extra = 3
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 3
+
+class TopicAdmin(admin.ModelAdmin):
+    # fields = ["pub_date", "topic_text"]
+    fieldsets = [
+        (None, {"fields": ["topic_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [CommentInline]
+    list_display = ["topic_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["topic_text"]
+
+
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Comment)
--- forum/models.py
+++ forum/models.py
@@ -2,6 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 
 class Topic(models.Model):
@@ -9,8 +10,14 @@ class Topic(models.Model):
     pub_date = models.DateTimeField("date published")
     def __str__(self):
         return self.topic_text
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Comment(models.Model):
